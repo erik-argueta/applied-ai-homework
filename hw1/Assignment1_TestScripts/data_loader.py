f9c672d43a9fe35035ee05aec26b893f4f43389d
Erik Argueta <argueta.erik@csu.fullerton.edu>
2026-09-08 18:19:26 -0700

    Initial progress hw1

---
+++ hw1/Assignment1_TestScripts/data_loader.py
@@ -0,0 +1,9 @@
+'''
+This file is where you migrate all the functions that you made in data_process1 and data_process2. 
+'''
+import numpy as np
+import pandas as pd
+import data_process1 
+
+# data_process1.load_dataset("/home/erik-csuf/Documents/applied/hw1/Assignment1_TestScripts/GasProperties.csv")
+data_process1.load_dataset_np("/home/erik-csuf/Documents/applied/hw1/Assignment1_TestScripts/GasProperties.csv")
